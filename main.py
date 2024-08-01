bf4f6cc4c3189231efe6f553f5c9fcc90c853e32
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-31 19:30:30 -0700

    movie class

---
+++ main.py
@@ -0,0 +1,11 @@
+class Movie:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+    def getId(self):
+        return self.id
+    
+    def getTitle(self):
+        return self.title
+    
