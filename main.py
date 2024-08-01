a66822c8cbe2bb5edd0ef45f1508675021b4b622
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-31 19:33:55 -0700

    movie rating

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ class User:
 
     def getId(self):
         return self.id
+    
+from enum import Enum
+
+class MovieRating(Enum):
+    NOT_RATED = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
