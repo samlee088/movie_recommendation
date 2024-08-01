ce4319b35d7c865e17cb0a24d04dce23c98fe63e
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-31 20:07:36 -0700

    RatingRegister class

--- main.py
+++ main.py
@@ -27,3 +27,39 @@ class MovieRating(Enum):
     FOUR = 4
     FIVE = 5
 
+class RatingRegister:
+    def __init__(self):
+        self.userMovies = {}
+        self.movieRatings = {}
+
+        self.movies = []
+        self.users = []
+
+    def addRating(self, user, movie, rating):
+        if movie.getId() not in self.movieRatings:
+            self.movieRatings[movie.getId()] = {}
+            self.movies.append(movie)
+        if user.getId() not in self.userMovies:
+            self.userMovies[user.getId()] = []
+            self.users.append(user)
+        self.userMovies[user.getId()].append(movie)
+        self.movieRatings[movie.getId()][user.getId()] = rating
+    
+    def getAverageRatings(self, movie):
+        if movie.getId() not in self.movieRatings:
+            return MovieRating.NOT_RATED.value
+        ratings = self.movieRatings[movie.getId()].values()
+        ratingValues = [rating.value for rating in ratings]
+        return sum(ratingValues) / len(ratings)
+    
+    def getUsers(self):
+        return self.users
+
+    def getMovies(self):
+        return self.movies
+    
+    def getUserMovies(self, user):
+        return self.userMovies.get(user.getIdI, [])
+    
+    def getMovieRatings(self, movie):
+        return self.movieRatings.get(movie.getIdId(), {})
